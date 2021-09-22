4aea5f156bfb90de7e9baa617d1cbe99379d9cc1
angularPracticas <hugoloyolamaya@gmail.com>
2021-09-22 09:53:14 -0500

    Se agrega em primer modulo 'Login' y dentro el componente loginHome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/**Modulos */
+import { LoginModule } from './login/login.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    LoginModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/login/login.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginHomeComponent } from './login-home/login-home.component';
+
+
+@NgModule({
+  declarations: [
+    LoginHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule
+  ],
+  exports: [
+    LoginHomeComponent
+  ]
+})
+export class LoginModule { }
